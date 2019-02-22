c9d7794b34f6e8a5aa5ffa4e2b108591e622944d
0xakk0r0kamui <hy@ninja.org>
2019-02-22 17:43:04 +0700

    Fix typo

---
+++ test/zkps/snprivacy.js
@@ -0,0 +1,28 @@
+let snprivacy = require('../../lib/zkps/snprivacy');
+let utils = require('../../lib/privacy_utils');
+let Pds = require('../../lib/pedersen').PedCom;
+let cs = require('../../lib/constants');
+let assert = require('assert');
+let P256 = require('../../lib/ec').P256;
+describe('Serial number privacy', function () {
+    let secretKey = null;
+    let publicKey = null;
+    let serialNumber = null;
+    let SND = null;
+    let wit = null;
+    let proof = null;
+    before(function () {
+        secretKey = utils.randScalar();
+        publicKey = P256.g.mul(secretKey).compress();
+    });
+    it('Serial number no privacy prove and verify with normal value', function () {
+        SND = utils.randScalar();
+        let rSND = utils.randScalar();
+        let rSK = utils.randScalar();
+        serialNumber = (Pds.G[cs.SK].derive(secretKey, SND));
+        wit = new snprivacy.SNPrivacyWitness();
+        wit.set(serialNumber, Pds.commitAtIndex(secretKey,rSK,cs.SK), Pds.commitAtIndex(SND,rSND,cs.SND), secretKey, rSK, SND, rSND);
+        proof = wit.prove();
+        assert.ok(proof.verify());
+    });
+});
