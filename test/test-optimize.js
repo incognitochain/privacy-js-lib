c22e0319a1a7e7f0b2dc093b661ce573285a65ae
0xkraken <hien.n@autonomous.nyc>
2019-03-20 15:42:59 +0700

    update

---
+++ test/test-optimize.js
@@ -0,0 +1,36 @@
+
+
+let P256 = require('../lib/ec').P256;
+let bn = require('bn.js');
+
+
+function Test123(){
+    let a = BigInt("2783456789034567894567895678905678");
+    let aBN = new bn("2783456789034567894567895678905678");
+
+    let sp = BigInt(20);
+    let spBN = new bn(20);
+
+    console.time("Method 1: ");
+    let result1 = P256.g.derive(spBN, aBN);
+    console.log("Result1: ", result1);
+    console.timeEnd("Method 1: ");
+
+    console.time("Method 2: ");
+    let result2 = P256.g.deriveOptimized(sp, a);
+    console.log("Result1: ", result2);
+    console.timeEnd("Method 2: ");
+
+
+    // console.time("Mul 1: ");
+    // let result1 = P256.g.mul(aBN);
+    // console.log("Result1: ", result1);
+    // console.timeEnd("Mul 1: ");
+
+    // console.time("Mul 2: ");
+    // let result2 = P256.g.mulOptimized(a);
+    // console.log("Result2: ", result2);
+    // console.timeEnd("Mul 2: ");
+}
+
+Test123()
