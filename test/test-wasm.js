93c2127e61f883a673d4a2c265df820759bcc4ee
0xkraken <hien.n@autonomous.nyc>
2019-06-19 17:25:49 +0700

    replace aggregated range prove to wasm

--- test/test-wasm.js
+++ test/test-wasm.js
@@ -4,6 +4,7 @@ require('golang-wasm-async-loader/lib/wasm_exec');
 require('isomorphic-fetch');
 const {readFileSync} = require('fs');
 var base64js = require('base64-js')
+var privacyUtils = require('../lib/privacy_utils');
 
 global.requestAnimationFrame = global.setImmediate;
 
@@ -11,18 +12,6 @@ let p = new Promise(resolve =>
   resolve(readFileSync(join(__dirname, '../privacy.wasm')))
 );
 const wasm = gobridge.default(p);
-
-function base64Decode(str){
-  let bytes = base64js.toByteArray(str);
-  console.log("bytes: ", bytes);
-  return bytes;
-}
-
-function base64Encode(bytesArray){
-  let str = base64js.fromByteArray(bytesArray);
-  console.log("str: ", str);
-  return str;
-}
 
 async function run() {
   let result = await wasm.add(1, 2);
@@ -37,12 +26,12 @@ async function run() {
   let proof = await wasm.aggregatedRangeProve(JSON.stringify(object));
 
   // console.log("proof base64 encode get from WASM: ", proof);
-  let proofBytes = base64Decode(proof);
+  let proofBytes = privacyUtils.base64Decode(proof);
 
   console.timeEnd("HHHHHHH time:");
   console.log("proofBytes: ", proofBytes.join(", "));
 
-  let proofEncode = base64Encode(proofBytes);
+  let proofEncode = privacyUtils.base64Encode(proofBytes);
   console.log("proofEncode: ", proofEncode);
 
 
