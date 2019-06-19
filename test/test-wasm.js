a240a50f9935a8476e58fbb2af22c44bd434c7b5
0xkraken <hien.n@autonomous.nyc>
2019-06-19 10:18:47 +0700

    update test wasm

--- test/test-wasm.js
+++ test/test-wasm.js
@@ -13,10 +13,11 @@ const wasm = gobridge.default(p);
 
 async function run() {
   let result = await wasm.add(1, 2);
-
   console.log(result);
-
   await wasm.sayHello("asfasf");
+
+  let proof = await wasm.aggregatedRangeProve({"values":["1","2"],"rands":["64","c8"]});
+  console.log("proof: ", proof);
 };
 
 run();
