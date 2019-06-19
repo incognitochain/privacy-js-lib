285ebf8aea3689b79f06397c74944d30c4f6824e
sirrush <thaibao@autonomous.nyc>
2019-06-19 13:34:56 +0700

    - update for wasm

--- test/test-wasm.js
+++ test/test-wasm.js
@@ -1,8 +1,8 @@
 const gobridge = require('golang-wasm-async-loader/dist/gobridge');
-const { join } = require('path');
+const {join} = require('path');
 require('golang-wasm-async-loader/lib/wasm_exec');
 require('isomorphic-fetch');
-const { readFileSync } = require('fs');
+const {readFileSync} = require('fs');
 
 global.requestAnimationFrame = global.setImmediate;
 
@@ -16,7 +16,7 @@ async function run() {
 
   console.log(result);
 
-  await wasm.sayHello("asfasf");
+  await wasm.sayHello("Bao");
 };
 
 run();
