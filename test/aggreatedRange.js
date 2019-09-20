55804c78f96965386ee4d5b5bcb858bbf091f925
0xkraken <hien.n@autonomous.nyc>
2019-09-20 11:08:35 +0700

    update param bullet proof

--- test/aggreatedRange.js
+++ test/aggreatedRange.js
@@ -1,12 +1,17 @@
 const {AggregatedRangeWitness, AggregatedRangeProof} = require('../lib/zkps/aggregatedrange');
 const bn = require("bn.js");
+const {randBytes, randScalar} = require('../lib/privacy_utils');
 
 async function TestAggregatedRange(){
-    // let values = [new bn('1000000'), new bn('2000000')];
-    let values = [new bn('1000000000000000'), new bn('2000000000000000')];
-    let rand = [new bn(10), new bn(20)];
+    let n = 10;
+    let values = new Array(n);
+    let rands = new Array(n)
+    for (let i=0; i<n; i++){
+        values[i] = new bn(randBytes(3));
+        rands[i] = randScalar();
+    }
     let witness = new AggregatedRangeWitness();
-    witness.set(values, rand);
+    witness.set(values, rands);
 
     let proof = await witness.prove();
 
